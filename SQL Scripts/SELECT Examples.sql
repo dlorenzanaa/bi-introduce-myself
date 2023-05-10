e46871b46649a81ae64ef7f507b292f61744bb15
Diana Lorenzana <126618559+dlorenzanaa@users.noreply.github.com>
2023-05-10 00:38:07 -0600

    Update SELECT Examples.sql

--- SQL Scripts/SELECT Examples.sql
+++ SQL Scripts/SELECT Examples.sql
@@ -12,3 +12,16 @@ WHERE CustomerId = 12345;
 SELECT SUM(OrderAmount) AS TotalGasto
 FROM Orders
 WHERE CustomerId = 12345;
+
+
+SELECT *
+FROM Deliveries
+WHERE OrderId = 56789;
+
+
+SELECT FirstName, LastName, Email
+FROM Customers
+WHERE Id IN (
+   SELECT DISTINCT CustomerId
+   FROM Orders
+);
