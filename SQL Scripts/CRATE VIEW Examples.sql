64b495acb04424f7e0c29946e45143f6ee6d5c42
Miguel Luevano <mike_luev@outlook.com>
2023-05-09 23:24:40 -0600

    Create CRATE VIEW Examples.sql

---
+++ SQL Scripts/CRATE VIEW Examples.sql
@@ -0,0 +1,5 @@
+CREATE VIEW Orders
+AS
+SELECT o.Id, o.OrderDate, o.OrderAmount, o.OrderStatus, c.FirstName + ' ' + c.LastName AS CustomerName
+FROM Orders o
+JOIN Customers c ON o.CustomerId = c.Id;
