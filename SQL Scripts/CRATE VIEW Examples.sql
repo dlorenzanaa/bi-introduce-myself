a0905b38a3a2fa68200ebee3fbc827aa225c9cd1
Diana Lorenzana <126618559+dlorenzanaa@users.noreply.github.com>
2023-05-10 00:03:41 -0600

    Update CRATE VIEW Examples.sql

--- SQL Scripts/CRATE VIEW Examples.sql
+++ SQL Scripts/CRATE VIEW Examples.sql
@@ -3,3 +3,9 @@ AS
 SELECT o.Id, o.OrderDate, o.OrderAmount, o.OrderStatus, c.FirstName + ' ' + c.LastName AS CustomerName
 FROM Orders o
 JOIN Customers c ON o.CustomerId = c.Id;
+
+
+CREATE VIEW VW_OrderInfo_BI 
+AS 
+SELECT Id, CustomerId, OrderDate, OrderAmount, OrderStatus, PaymentMethod
+FROM Orders;
