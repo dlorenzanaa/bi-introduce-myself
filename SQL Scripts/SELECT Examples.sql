5bb6f3f4829aab9706cf718a65eab871e38dd46b
Diana Lorenzana <126618559+dlorenzanaa@users.noreply.github.com>
2023-05-10 00:32:17 -0600

    Update SELECT Examples.sql

--- SQL Scripts/SELECT Examples.sql
+++ SQL Scripts/SELECT Examples.sql
@@ -2,3 +2,13 @@ SELECT o.Id, o.OrderDate, o.OrderAmount, o.OrderStatus, c.FirstName + ' ' + c.La
 FROM Orders o
 JOIN Customers c ON o.CustomerId = c.Id
 WHERE o.OrderStatus = 'Delivered';
+
+
+SELECT *
+FROM Orders
+WHERE CustomerId = 12345;
+
+
+SELECT SUM(OrderAmount) AS TotalGasto
+FROM Orders
+WHERE CustomerId = 12345;
