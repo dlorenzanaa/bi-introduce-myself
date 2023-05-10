9c54ddf5383325b4384427bf1f73315ea2e53024
Diana Lorenzana <126618559+dlorenzanaa@users.noreply.github.com>
2023-05-10 00:07:30 -0600

    Update CRATE VIEW Examples.sql

--- SQL Scripts/CRATE VIEW Examples.sql
+++ SQL Scripts/CRATE VIEW Examples.sql
@@ -18,3 +18,11 @@ SELECT o.Id, o.CustomerId, o.OrderDate, o.OrderAmount, o.OrderStatus, o.PaymentM
        c.City, c.State, c.PostalCode
 FROM Orders o
 JOIN Customers c ON o.CustomerId = c.Id;
+
+CREATE VIEW VW_TotalSpentByCustomer_BI 
+AS 
+SELECT c.Id, c.FirstName, c.LastName, c.Email, c.PhoneNumber, c.TotalSpent
+FROM Customers c
+JOIN (SELECT CustomerId, SUM(OrderAmount) AS TotalSpent FROM Orders GROUP BY CustomerId) o
+ON c.Id = o.CustomerId;
+
