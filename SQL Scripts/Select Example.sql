01a60c1d7179542576d7fa6af48c16cbb4578cab
Miguel Luevano <mike_luev@outlook.com>
2023-05-09 23:23:09 -0600

    Update Select Example.sql

--- SQL Scripts/Select Example.sql
+++ SQL Scripts/Select Example.sql
@@ -1,4 +1,4 @@
-SELECT
-	*
-FROM
-	[dbo].[Inventory]
+SELECT o.Id, o.OrderDate, o.OrderAmount, o.OrderStatus, c.FirstName + ' ' + c.LastName AS CustomerName
+FROM Orders o
+JOIN Customers c ON o.CustomerId = c.Id
+WHERE o.OrderStatus = 'Delivered';
