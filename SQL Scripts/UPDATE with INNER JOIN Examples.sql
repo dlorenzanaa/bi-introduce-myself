43bd86c34a49b60c09efc22067cc373d837a556b
Diana Lorenzana <126618559+dlorenzanaa@users.noreply.github.com>
2023-05-10 00:50:30 -0600

    Update UPDATE with INNER JOIN Examples.sql

--- SQL Scripts/UPDATE with INNER JOIN Examples.sql
+++ SQL Scripts/UPDATE with INNER JOIN Examples.sql
@@ -3,3 +3,30 @@ SET c.TotalSpent = c.TotalSpent + o.OrderAmount,
     c.TotalOrders = c.TotalOrders + 1
 FROM Customers c
 JOIN Orders o ON c.Id = o.CustomerId;
+
+
+UPDATE o
+SET o.OrderStatus = 'Sent', d.DeliveryStatus = 'Sent'
+FROM Orders o
+INNER JOIN Deliveries d ON o.Id = d.OrderId
+WHERE o.OrderStatus = 'Pending' AND d.DeliveryStatus = 'Scheduled'
+
+
+UPDATE c
+SET c.TotalSpent = (SELECT SUM(OrderAmount) FROM Orders WHERE CustomerId = c.Id)
+FROM Customers c
+INNER JOIN Orders o ON c.Id = o.CustomerId
+
+
+UPDATE o
+SET o.PaymentMethod = 'Credit Card'
+FROM Orders o
+INNER JOIN Customers c ON o.CustomerId = c.Id
+WHERE o.PaymentMethod = 'PayPal' AND c.TotalSpent > 1000
+
+
+UPDATE o
+SET o.ShippingAddress = d.DeliveryAddress
+FROM Orders o
+INNER JOIN Deliveries d ON o.Id = d.OrderId
+WHERE o.ShippingMethod = 'Delivery' AND o.ShippingAddress <> d.DeliveryAddress
